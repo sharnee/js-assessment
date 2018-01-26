56448cfc4efb63b5ee55e06bfc21d7fcc8108cba
Shar'nee Francis <shareefrancis11@gmail.com>
2018-01-26 15:04:12 -0600

    reversed the string

--- app/strings.js
+++ app/strings.js
@@ -22,6 +22,6 @@ stringsAnswers = {
    * @returns {String} The original string of text str reversed.
    */
   reverseString: function reverseString(str) {
-
+   return str.split("").reverse().join("")
   },
 };
