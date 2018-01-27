34fae84bfffeef8ca55146f16bcce2b4ff28c6f3
Shar'nee Francis <shareefrancis11@gmail.com>
2018-01-26 19:52:46 -0600

    created a function that returns a sting plus a space plus the function arg

--- app/functions.js
+++ app/functions.js
@@ -27,6 +27,8 @@ functionsAnswers = {
    * that begins with str + ', ' + that 1 argument.
    */
   functionFunction: function functionFunction(str) {
-
+     return function(arg) {
+        return str + ', ' + arg;
+      };
   },
 };
